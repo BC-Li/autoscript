5be61dbd855cd9a6d8d2ba5136d501215b7d58c6
BC-Li <251156266@qq.com>
2021-09-22 17:01:10 +0800

    [code] add autopull.sh

---
+++ linux/autopull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd path-to-your-courseware\2021fall-class-materials
+# If you unintentionally make some changes to the slides and want to erase all the changes you made to the slides, uncomment the code below.
+# NOTE THAT THE LINES BELOW WILL MAKE ALL YOUR FILES NEW. PLEASE BACKUP FIRST!!!
+# Do not uncomment this line and the lines above. 
+# git fetch --all
+# git reset --hard origin/master
+git pull
